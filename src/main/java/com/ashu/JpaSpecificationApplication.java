820f42b04610129052da0a70f3f1375b63d17736
Ashutosh Sahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2018-08-20 01:14:26 +0530

    Initial commit

---
+++ src/main/java/com/ashu/JpaSpecificationApplication.java
@@ -0,0 +1,18 @@
+package com.ashu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot entry point.
+ * 
+ * @author ashutosh
+ *
+ */
+@SpringBootApplication
+public class JpaSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpecificationApplication.class, args);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `products` (`id`, `name`, `price`, `color`, `added_date`) VALUES
+(11197, 'tea', 31.5, 'white', '2014-07-16 14:08:13'),
+(11219, 'coffee', 40.5, 'black', '2014-05-16 14:15:21'),
+(11220, 'tea', 45.5, 'brown', '2014-09-16 14:15:21'),
+(11221, 'milk', 31.5, 'white', '2014-06-16 14:15:21');
